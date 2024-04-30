e80a52e785029c564aed76379982636dabedfec2
cocobongo <davce21_89@hotmail.com>
2024-04-30 11:08:29 -0500

    Creates src directory Move app.js to src

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const port = 3000;
+
+/* setup static file folders */
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+  res.render("index.html");
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
