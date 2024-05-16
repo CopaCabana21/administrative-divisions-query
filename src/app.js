24f71472d80d2421ba7858aa57c245be775807f6
cocobongo <davce21_89@hotmail.com>
2024-05-16 17:20:31 -0500

    Adds addSelectionStructure.json and use it to make tree selection

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import fs from 'fs';
 
 const app = express();
 const port = 3000;
@@ -6,8 +7,13 @@ const port = 3000;
 /* setup static file folders */
 app.use(express.static('public'));
 
+const addSelectionStructure = JSON.parse(fs.readFileSync("./src/data/add/288247/addSelectionStructure.json"));
+
+// console.log(addSellectionTags);
+
+
 app.get("/", (req, res) => {
-  res.render("index.html");
+  res.render("index.ejs", {addSelectionStructure: addSelectionStructure});
 })
 
 app.listen(port, () => {
