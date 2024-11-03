4f61e4592a19a81cb154eace2cbabe9644880765
cocobongo <davce21_89@hotmail.com>
2024-11-02 19:41:41 -0500

    Modify the setup to test index.html locally

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 import express from "express";
 import fs from 'fs';
+import path from "path";
 
 const app = express();
 const port = 3000;
 
 /* setup static file folders */
-app.use(express.static('public'));
+// for index.ejs
+// app.use(express.static('public'));
+// for index.html
+app.use(express.static('.'));
 
-const addTagsTree = JSON.parse(fs.readFileSync("./data/add/addsDataMinJoinedTree.json"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs", {addSelectionStructure: addTagsTree});
+
+    // for index.ejs
+    // const addTagsTree = JSON.parse(fs.readFileSync("./data/add/addsDataMinJoinedTree.json"));
+    // res.render("index.ejs", {addSelectionStructure: addTagsTree});
+    
+    // for index.html
+    res.sendFile("index.html", {root: '.'});
 })
 
 app.listen(port, () => {
