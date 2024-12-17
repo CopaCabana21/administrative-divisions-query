c5c6862841c53d421c9b343488d360680f29a36f
cocobongo <davce21_89@hotmail.com>
2024-12-17 12:49:12 -0500

    Move globe-americas.svg to src folder and use copy-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 import path, { resolve } from 'path';
 import { fileURLToPath } from 'url';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -30,11 +31,23 @@ export default {
                     'css-loader'
                 ],
             },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/,
+                type: 'asset/resource', // the asset module treat it as a resource
+                generator: {
+                    filename: 'assets/images/[name][hash][ext][query]',
+                },
+            },
         ],
     },
     plugins: [
         new MiniCssExtractPlugin({
             filename: 'styles.css',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/assets/images', to: 'assets/images' }
+            ],
         }),
     ],
 };
